cd4e0d8a72145150a629f821aae774bdea2fee77
Andrea Cerone <andrea.cerone@gmail.com>
2020-02-19 17:47:19 +0000

    Option exercises - part 1

--- exercises/src/main/scala/fpinscala/errorhandling/Option.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Option.scala
@@ -4,15 +4,32 @@ package fpinscala.errorhandling
 import scala.{Option => _, Some => _, Either => _, _} // hide std library `Option`, `Some` and `Either`, since we are writing our own in this chapter
 
 sealed trait Option[+A] {
-  def map[B](f: A => B): Option[B] = ???
 
-  def getOrElse[B>:A](default: => B): B = ???
+  def map[B](f: A => B): Option[B] = this match {
+    case None => None
+    case Some(value) => Some(f(value))
+  }
 
-  def flatMap[B](f: A => Option[B]): Option[B] = ???
+  def getOrElse[B>:A](default: => B): B = this match {
+    case None => default
+    case Some(value) => value
+  }
 
-  def orElse[B>:A](ob: => Option[B]): Option[B] = ???
+  def flatMap[B](f: A => Option[B]): Option[B] = this match {
+    case None => None
+    case Some(value) => f(value)
+  }
 
-  def filter(f: A => Boolean): Option[A] = ???
+  def orElse[B>:A](ob: => Option[B]): Option[B] = this match {
+    case None => ob
+    case Some(value) => Some(value)
+  }
+
+  def filter(f: A => Boolean): Option[A] = this match {
+    case None => None
+    case Some(a) if !f(a) => None
+    case Some(a) => Some(a)
+  }
 }
 case class Some[+A](get: A) extends Option[A]
 case object None extends Option[Nothing]
@@ -38,11 +55,14 @@ object Option {
   def mean(xs: Seq[Double]): Option[Double] =
     if (xs.isEmpty) None
     else Some(xs.sum / xs.length)
-  def variance(xs: Seq[Double]): Option[Double] = ???
+  def variance(xs: Seq[Double]): Option[Double] = mean(xs).flatMap { m => mean(xs.map(x => math.pow(x - m, 2))) }
 
-  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = ???
+  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = a.flatMap(a => b.map(b => f(a,b)))
 
-  def sequence[A](a: List[Option[A]]): Option[List[A]] = ???
+  def sequence[A](a: List[Option[A]]): Option[List[A]] =
+    a.foldRight(Some(Nil): Option[List[A]]){ case (nextElement, partialList) =>
+      map2(nextElement, partialList)(_ :: _)
+  }
 
   def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = ???
 }
